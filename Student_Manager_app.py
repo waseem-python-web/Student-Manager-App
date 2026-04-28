559a1b5a9792b8941245aacb4130655f8e3849da
waseempython project <mianwaseempakpython@gmail.com>
2026-04-28 00:51:30 -0700

    Add files via upload

---
+++ Student_Manager_app.py
@@ -0,0 +1,46 @@
+Student = {}
+
+while True:
+    print("===== Student Manager App =====")
+    print("1. Add name")
+    print("2. View students")
+    print("3. Check result")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        name = input("Enter your name: ")
+        marks = int(input("Enter your marks: "))
+
+        if marks < 0 or marks > 100:
+            print("Invalid marks! Enter between 0-100")
+            continue
+
+        Student[name] = marks
+        print(f"{name} added successfully")
+
+    elif choice == "2":
+        if not Student:
+            print("No students added")
+        else:
+            for name, marks in Student.items():
+                print(name, ":", marks)
+
+    elif choice == "3":
+        name = input("Enter your name: ")
+        if name in Student:
+            marks = Student[name]
+            if marks >= 40:
+                print("Pass")
+            else:
+                print("Fail")
+        else:
+            print("Student not found")
+
+    elif choice == "4":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice")
